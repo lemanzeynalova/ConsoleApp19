acf2e784513eb48618e1dbe9b374885ff85fdec6
User <User@DESKTOP-Q9QV65T>
2023-11-02 23:31:13 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User(1, "Leman");
+            user.Id = 2;
+            user.Name = "Ugur";
+            Console.WriteLine("User Information:");
+            Console.WriteLine("ID: " + user.Id);
+            Console.WriteLine("Name: " + user.Name);
+            Console.WriteLine("Age: " + User.Age);
+
+
+            User.ChangeAge(30);
+
+
+            Console.WriteLine("Yasi deyisdikden sonra istifadeci melumatlari");
+            Console.WriteLine("ID: " + user.Id);
+            Console.WriteLine("Name: " + user.Name);
+            Console.WriteLine("Age: " + User.Age);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    public class User
+    {
+        private int id;
+        private string name;
+        private static int age;
+
+        public User(int id, string name) 
+        {
+        this.id = id;
+        this.name = name;
+        }
+        public int Id
+        {
+            get { return id; } 
+            set { id = value; }
+        } 
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public static int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public static void ChangeAge(int newAge)
+        {
+            age = newAge;
+        }
+        
+
+    }
+}
